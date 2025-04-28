2f21b1bcdee2ffba9aafa6d7a640a9757b352977
Mahendiran <mahi@Mahendirans-MacBook-Pro.local>
2025-04-28 21:09:54 +0530

    Added Test for login page negative scenarios

--- src/pages/login.page.ts
+++ src/pages/login.page.ts
@@ -5,12 +5,14 @@ export class LoginPage {
   readonly usernameInput: Locator;
   readonly passwordInput: Locator;
   readonly loginButton: Locator;
+  readonly errorToast: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.usernameInput = page.getByPlaceholder('Username');
     this.passwordInput = page.getByPlaceholder('Password');
     this.loginButton = page.getByText('Login');
+    this.errorToast = page.locator("[data-test='error']");
   }
 
   async navigateToSwagLabs(URL: string) {
@@ -20,5 +22,9 @@ export class LoginPage {
     await this.usernameInput.fill(username);
     await this.passwordInput.fill(password);
     await this.loginButton.click();
+  }
+
+  async validateErrorToastMessage() {
+    return await this.errorToast.textContent();
   }
 }
--- src/tests/e2e.spec.ts
+++ src/tests/e2e.spec.ts
@@ -3,7 +3,6 @@ import { USER_NAME, PASSWORD, APPLICATION_URL, FIRST_NAME, SECOND_NAME, POSTAL_C
 import { InventoryPage } from "../pages/inventory.page";
 import { CheckoutPage } from "../pages/checkout.page";
 import { LoginPage } from "../pages/login.page";
-import { log } from "console";
 
 test.describe("Sauce Demo E2E Test", () => {
   let inventoryPage: InventoryPage;
@@ -28,6 +27,29 @@ test.describe("Sauce Demo E2E Test", () => {
       const totalPrice = await inventoryPage.addProductsToCart(); 
       await inventoryPage.navigateToCart();
       await checkoutPage.performCheckout(totalPrice, FIRST_NAME, SECOND_NAME, POSTAL_CODE);
+    });
+  });
+  test("Login with Invalid Password", async () => {
+    await test.step("Login to Swag Labs", async () => {
+      await loginPage.navigateToSwagLabs(APPLICATION_URL);
+      await loginPage.login(USER_NAME, "");
+      expect(await loginPage.validateErrorToastMessage()).toBe("Epic sadface: Password is required");
+    });
+  });
+
+  test("Login with Invalid UserName", async () => {
+    await test.step("Login to Swag Labs", async () => {
+      await loginPage.navigateToSwagLabs(APPLICATION_URL);
+      await loginPage.login("", PASSWORD);
+      expect(await loginPage.validateErrorToastMessage()).toBe("Epic sadface: Username is required");
+    });
+  });
+
+  test("Login with both Invalid UserName & Password", async () => {
+    await test.step("Login to Swag Labs", async () => {
+      await loginPage.navigateToSwagLabs(APPLICATION_URL);
+      await loginPage.login("invalid", "invalid");
+      expect(await loginPage.validateErrorToastMessage()).toBe("Epic sadface: Username and password do not match any user in this service");
     });
   });
 });
